10fdc90f54713693c19d58519d410036c5af15cc
edri <mig.santamar@gmail.com>
2015-10-26 15:50:42 +0100

    Ajout du contrôleur User comme contrôleur par défaut.

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -17,7 +17,7 @@ return array(
                 'options' => array(
                     'route'    => '/',
                     'defaults' => array(
-                        'controller' => 'Application\Controller\Index',
+                        'controller' => 'Application\Controller\User',
                         'action'     => 'index',
                     ),
                 ),
