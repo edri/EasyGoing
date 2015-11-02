e20d04bf8fde45a67fa962665535833a2e47d196
edri <mig.santamar@gmail.com>
2015-11-02 13:59:29 +0100

    Ajout d'une implémentation avec la DB (encore non-fonctionnel).

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
  return array(
      'db' => array(
          'driver'         => 'Pdo',
-         'dsn'            => 'mysql:dbname=easygoing;host=localhost',
+         'dsn'            => 'mysql:unix_socket=/opt/lampp/var/mysql/mysql.sock;mysql:dbname=easygoing;host=localhost',
          'driver_options' => array(
              PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
          ),
@@ -26,4 +26,3 @@
          ),
      ),
  );
- 
--- module/Application/src/Application/Controller/UserController.php
+++ module/Application/src/Application/Controller/UserController.php
@@ -21,13 +21,33 @@ use Zend\Session\Container;
 // Be careful about the class' name, which must be the same as the file's name.
 class UserController extends AbstractActionController
 {
+	// The user's model used to communicate with the database.
+	private $userTable;
+
+	// Get the user's table's entity, represented by the created model.
+	// Act as a singleton : we only can have one instance of the object.
+	private function getUserTable()
+	{
+		// If the object is not currencly instanciated, we do it.
+		if (!$this->userTable) {
+			$sm = $this->getServiceLocator();
+			// Instanciate the object with the created model.
+			$this->userTable = $sm->get('Application\Model\UserTable');
+		}
+		return $this->userTable;
+	}
+
 	// Default action of the controller.
 	// In normal case, it will be calling when the user access the "mySite.com/myController/" page,
 	// but here we are in the default controller so the page will be "mySite.com/".
 	public function indexAction()
 	{
+		$test = "COUCOU";//$this->getUserTable()->checkCreditentials("raphaelracine", "d74ff0ee8da3b9806b18c877dbf29bbde50b5bd8e4dad7a3a725000feb82e8f1") ? "OUIIII" : "NON !";
+
 		// For linking the right action's view.
-		return new ViewModel();
+		return new ViewModel(array(
+			'test'	=>	$test
+		));
 	}
 	public function registrationAction()
 	{
