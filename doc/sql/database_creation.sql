ce8c030e23e7b41ae28850c516d48d417e1984de
Raphaël Racine <raphaelracine@users.noreply.github.com>
2015-11-09 13:32:14 +0100

    Order by p.name

--- doc/sql/database_creation.sql
+++ doc/sql/database_creation.sql
@@ -151,12 +151,15 @@ CREATE TABLE usersTasksProductions
 /* This view show all the projects with all members id which are in the project */
 CREATE VIEW view_projects_min AS
 (
-	  SELECT p.id, p.name, p.fileLogo, pu.user AS userId, pu.isAdmin
-	  FROM projectsUsersMembers as pu
+	SELECT p.id, p.name, p.fileLogo, pu.user AS userId, pu.isAdmin
+	FROM projectsUsersMembers as pu
 		INNER JOIN projects AS p ON p.id = pu.project
+	ORDER BY p.name	 
 );
 
 /* Stored procedures and functions */
+
+
 
 /* This function check if a user can be affected to a task */
 USE easygoing;
