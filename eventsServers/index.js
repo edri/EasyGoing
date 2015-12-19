78b690b8cfc804a0dc8967aa5855dbe9790e373c
edri <mig.santamar@gmail.com>
2015-12-19 20:14:44 +0100

    Refactoring.

--- eventsServers/index.js
+++ eventsServers/index.js
@@ -8,13 +8,13 @@ const HTTP_PORT = 8002;
 
 // Create websocket server.
 var socketServer = ws.createServer(function(connection) {
-	console.log("WEBSOCKET: New client's connection!");
+	console.log("WEBSOCKET: new client's connection!");
 	// Init current connection's project's ID to null.
 	connection.projectId = null;
 
 	// Triggered when the server received data from client.
 	connection.on("text", function(str) {
-		console.log("WEBSOCKET: Received message: '" + str + "'");
+		console.log("WEBSOCKET: received message: '" + str + "'");
 
 		try {
 			// Try to parse received data to JSON.
@@ -23,38 +23,38 @@ var socketServer = ws.createServer(function(connection) {
 			// Do some actions, depending on the received data message's type.
 			switch (data.messageType) {
 				case "projectListeningRequest":
-					console.log("WEBSOCKET: Adding current connection to project #" + data.projectId + "...");
+					console.log("WEBSOCKET: adding current connection to project #" + data.projectId + "...");
 					// Set current connection's project.
 					connection.projectId = data.projectId;
 					break;
 				case "taskMoving":
 					sendTaskMovingSocket(data, connection);
-					sendEvent(data.event);
+					sendEventSocket(data.event);
 					break;
 			}
 		}
 		catch(e) {
-			console.log("WEBSOCKET: Invalid data format, please send JSON.");
+			console.log("WEBSOCKET: an error occured.");
 			console.log(e);
 		}
 	});
 
 	connection.on("close", function(code, reason) {
-		console.log("WEBSOCKET: A client left.");
+		console.log("WEBSOCKET: a client left the server.");
 	});
 }).listen(SOCKET_PORT);
 
 console.log("Websocket server listening on: ws://127.0.0.1:%s", SOCKET_PORT);
 
 // Send a new event to every connected users that currently are in the concerned project.
-function sendEvent(eventData) {
+function sendEventSocket(eventData) {
 	var newEventData = {
 		"messageType": "newEvent",
 		"event": eventData
 	};
 
 	socketServer.connections.forEach(function(connection) {
-		console.log("WEBSOCKET: Send new-event socket to every concerned clients...");
+		console.log("WEBSOCKET: send new-event socket to every concerned clients...");
 
 		// Send event socket to everyone in the project.
 		if (connection.projectId == eventData.project) {
@@ -88,18 +88,17 @@ function handleRequest(req, res) {
 		var form = new formidable.IncomingForm();
 		// Parse form data to get JSON.
 		form.parse(req, function(err, fields, files) {
-			console.log("HTTP: Received HTTP POST request:");
+			console.log("HTTP: received HTTP POST request:");
 			console.log(fields);
 
 			// Do some actions, depending on the received data message's type.
 			switch (fields.requestType) {
 				case "newEvent":
-					console.log("HTTP: Received a new event.");
+					console.log("HTTP: received a new event.");
 					// Parse event's data to JSON.
 					var event = JSON.parse(fields.event);
-
-					sendEvent(event);
-
+					// Send the received event to all concerned clients.
+					sendEventSocket(event);
 					break;
 			}
 
@@ -110,7 +109,7 @@ function handleRequest(req, res) {
 	// Otherwise we send back an error to the client.
 	else {
 		response.writeHead(405, "Method not supported", {'Content-Type': 'text/html'});
-    	response.end('<html><head><title>405 - Method not supported</title></head><body><h1>Method not supported.</h1></body></html>');
+		response.end('<html><head><title>405 - Method not supported</title></head><body><h1>Method not supported.</h1></body></html>');
 	}
 }
 
