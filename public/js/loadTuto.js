6c9f42f858e4fa9e16656a2e6119c6de7066885a
Raphaël Racine <raphaelracine@users.noreply.github.com>
2015-12-27 11:02:48 +0100

    tuto

--- public/js/loadTuto.js
+++ public/js/loadTuto.js
@@ -7,22 +7,26 @@ var skipTutorial = function() {
 
 var nextTutorial = function() {
 
-	if(tutoData.length != 0) { 
+	if(tutoData.length != 0) { // To avoid overflow... else we have first : d = tutoData[0] and overflow
 		
         var d = tutoData[current];
         
         if(current < tutoData.length) {
-
+            
+            // Template html for tutorial and show the current tuto
             $("div[role=tutorial][id = " + d.div + "]").html(
                 "<img src=/img/tuto.png /> " + 
                 d.text + 
                 "<a href='#' onClick='nextTutorial()'>   <br>Next</a>" +
                 "<a href='#' onClick='skipTutorial()'><br>Skip</a>"
             ).fadeIn();
+            
+            // Hide all of other tutos
             $("div[role=tutorial][id != " + d.div + "]").fadeOut();
             current++;
         }
         else {
+            // Hide all of the tutos
             $("div[role=tutorial]").fadeOut();
         }
 	}
@@ -31,45 +35,37 @@ var nextTutorial = function() {
 
 function loadTutorial(tuto) {
 	
-	/*<?php
-		if($sessionUser->wantTutorial) {
-	?>*/
-	
-		Tutorial(tuto, function(data) {
-		
-			var tutoDataFiltered = [];
-		
-			data.forEach(function(d) {
-				$("div[role=tutorial][id=" + d.div + "]").each(function() {
+    Tutorial(tuto, function(data) {
 
-					var div = $(this);
+        var tutoDataFiltered = [];
 
-					tutoData.push(d);
+        data.forEach(function(d) {
+            $("div[role=tutorial][id=" + d.div + "]").each(function() {
 
-					var position = div.position();
+                var div = $(this);
 
-					div.css({
-						position: 'absolute',
-						top: position.top,
-						left: position.left,
-						width: position.width,
-						height: position.heigth,
-						paddingLeft: 25,
-						paddingRight: 25,
-						paddingBottom: 10,
-						paddingTop: 10,
-						borderRadius: 10,
-						background: "lightyellow",
-						borderStyle: "solid"
-					});
-				});
-			});
+                tutoData.push(d);
 
-			nextTutorial();
-		});
-	
-	
-	/*<?php
-		}
-	?>*/
+                var position = div.position();
+
+                div.css({
+                    position: 'absolute',
+                    top: position.top,
+                    left: position.left,
+                    width: position.width,
+                    height: position.heigth,
+                    paddingLeft: 25,
+                    paddingRight: 25,
+                    paddingBottom: 10,
+                    paddingTop: 10,
+                    borderRadius: 10,
+                    background: "lightyellow",
+                    borderStyle: "solid"
+                });
+            });
+        });
+
+        nextTutorial();
+    });
+
 };
