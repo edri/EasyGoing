8af1bd641cacd50e05f41c23f4c56ae947cc1c19
edri <mig.santamar@gmail.com>
2015-12-29 20:51:55 +0100

    Add mandatory fields.

--- module/Application/src/Application/Controller/ProjectController.php
+++ module/Application/src/Application/Controller/ProjectController.php
@@ -467,10 +467,6 @@ class ProjectController extends AbstractActionController
              'id' => $projectId
          ));
       }
-
-      return new ViewModel(array(
-         'isSubTask' => $this->params('otherId') ? true : false
-      ));
    }
 
    public function taskDetailsAction()
