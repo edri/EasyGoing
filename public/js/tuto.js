c01db39a26604160c5bf181b128ef8e30478a519
Raphaël Racine <raphaelracine@users.noreply.github.com>
2016-01-02 10:12:39 +0100

    simplify tuto.js

--- public/js/tuto.js
+++ public/js/tuto.js
@@ -1,10 +1,52 @@
-var Tutorial = function(tuto, callback) {
+var tutoData = [];
+var current = 0;
 
+var skipTutorial = function() {
+	$("div[role=tutorial]").tooltip('hide');
+};
+
+var nextTutorial = function() {
+
+	if(tutoData.length != 0) { // To avoid overflow... else we have first : d = tutoData[0] and overflow
+		
+        var d = tutoData[current];
+        
+        if(current < tutoData.length) {
+            
+            // Template html for tutorial and show the current tuto
+            $("div[role=tutorial][id = " + d.div + "]")
+                .attr("data-html", "true")
+                .attr("title", d.text + "<a href=# onClick=nextTutorial() ><br>Next</a>"
+                     + "<a href=# onClick=skipTutorial() ><br>Skip</a>")
+                .tooltip('show');
+            
+            // Hide all of other tutos
+            $("div[role=tutorial][id != " + d.div + "]").tooltip('hide');
+            current++;
+        }
+        else {
+            // Hide all of the tutos
+            skipTutorial();
+        }
+	}
+
+};
+
+function loadTutorial(tuto) {
+    
     $.ajax({            
         type: 'GET',
         url: '/tutorial/' +  tuto,
         dataType: 'json',
-        success: callback
+        success: function(data) {
+        var tutoDataFiltered = [];
+
+            data.forEach(function(d) {             
+                tutoData.push(d);
+            });
+
+            nextTutorial();
+        }
     });
 
 };
