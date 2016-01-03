6d7b02d770063bae74e8f63b248c5b1dc0803c9d
edri <mig.santamar@gmail.com>
2016-01-03 03:28:37 +0100

    Rendering subtasks.

--- module/Application/src/Application/Model/ViewUsersTasks.php
+++ module/Application/src/Application/Model/ViewUsersTasks.php
@@ -23,7 +23,7 @@ class ViewUsersTasks
       $this->name  = (!empty($data['name'])) ? $data['name'] : null;
       $this->description  = (!empty($data['description'])) ? $data['description'] : null;
       $this->deadLineDate  = (!empty($data['deadLineDate'])) ? $data['deadLineDate'] : null;
-      $this->durationInHours  = (!empty($data['durationInHours'])) ? $data['durationInHours'] : null;
+      $this->durationInHours  = (!empty($data['durationsInHours'])) ? $data['durationsInHours'] : null;
       $this->priorityLevel  = (!empty($data['priorityLevel'])) ? $data['priorityLevel'] : null;
       $this->state  = (!empty($data['state'])) ? $data['state'] : null;
       $this->project  = (!empty($data['project'])) ? $data['project'] : null;
